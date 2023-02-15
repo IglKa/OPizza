d3e8a7d94ebdcd8729fdaf750fe2322428447596
Игорь <superigorek74@gmail.com>
2023-02-16 00:26:46 +0500

    refactore

--- OPizza/restaurant/views/restaurant.py
+++ OPizza/restaurant/views/restaurant.py
@@ -5,14 +5,14 @@ from restaurant.models import Restaurant
 
 class RestaurantListView(ListView):
     model = Restaurant
-    context_object_name = 'rstr'
+    context_object_name = 'restaurants'
     template_name = 'base.html'
 
 
 class RestaurantDetailView(DetailView):
     model = Restaurant
     template_name = 'restaurant/detail.html'
-    context_object_name = 'rstr'
+    context_object_name = 'restaurant'
 
     # def get(self, request, *args, **kwargs):
     #     meals = find_meals()
